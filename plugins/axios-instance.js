0c0ee7c84f5d78ced1a80aad9dc57f2bdaeed233
ChKhasan <chulliyevhasan1405@mail.ru>
2024-04-01 13:49:11 +0500

    first commit 2

--- plugins/axios-instance.js
+++ plugins/axios-instance.js
@@ -1,7 +1,7 @@
 export default ({ $axios, redirect, error }, inject) => {
   const tokenKey = "auth_token";
   const axiosInstance = $axios.create({
-    baseURL: process.env.BASE_URL || 'https://test-api.hirelancer.ndc.uz',
+    baseURL: process.env.BASE_URL || 'https://api.logistic.ndc.uz',
   });
   // axiosInstance.setHeader("Content-Type", "application/json");
 
